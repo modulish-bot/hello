8f3a9d11f203fe11ea2f68553b75ffe86c3abe2d
Daniel <danilacasito8@gmail.com>
2025-09-06 23:21:40 +0200

    Add project files

---
+++ hello.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+class Hello(commands.Cog):
+    def __init__(self, bot, config_dir: str):
+        self.bot = bot
+
+    @discord.slash_command(name="hello")
+    async def hello(self, ctx):
+        await ctx.send("Hello, world!")
+
+def setup(bot: commands.Bot, config_dir: str = None):
+    if config_dir is None:
+        config_dir = globals().get('PLUGIN_CONFIG_DIR', '<unknown>')
+    bot.add_cog(Hello(bot, config_dir))
